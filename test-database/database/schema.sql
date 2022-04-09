26c8c6fadb5cde37fd7560c280a137339ebf4822
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-09 02:30:51 -0700

    Added almost all of Joker's frame data

--- test-database/database/schema.sql
+++ test-database/database/schema.sql
@@ -9,21 +9,32 @@ create schema "public";
 CREATE TABLE public.characters(
   "id" SERIAL NOT NULL,
   "name" VARCHAR(500) NOT NULL,
-  "rosterId" INT NOT NULL,
+  "rosterId" VARCHAR NOT NULL,
   "displayName" VARCHAR(500) NOT NULL,
   "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT NOW(),
   PRIMARY KEY("id")
 );
 
-CREATE TABLE public.data(
-  "id" SERIAL REFERENCES public.characters,
-  "moves" JSONB NOT NULL,
-  -- "name" VARCHAR(500) NOT NULL,
-  -- "activeFrames" TEXT NOT NULL,
-  -- "damage" TEXT NOT NULL,
-  -- "totalFrames" TEXT NOT NULL,
-  "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT NOW()
+
+CREATE TABLE public.moves(
+  "moveId" SERIAL NOT NULL,
+  "name" VARCHAR(50) NOT NULL,
+  "damage" VARCHAR(50) NOT NULL,
+  "activeFrames" VARCHAR(50) NOT NULL,
+  "totalFrames" VARCHAR(50) NOT NULL,
+  "hitboxType" VARCHAR(50) NOT NULL,
+  "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT NOW(),
+  PRIMARY KEY("moveId")
 );
+
+CREATE TABLE public.characterData(
+  "id" int NOT NULL,
+  -- "throwId" SERIAL REFERENCES public.throws,
+  -- "attributeid" SERIAL REFERENCES public.attributes,
+  -- "miscellaneousId" SERIAL REFERENCES public.miscellaneous,
+  "moveId" SERIAL REFERENCES public.moves
+);
+
 -- CREATE TABLE public.purchases
 -- (
 --     id   serial PRIMARY KEY,
