a325b783ae9f8330d07d2682efedc89799737ac2
DreSean-Sutton <dreseansutton@gmail.com>
2023-07-17 05:38:53 -0700

    In progress

--- python/car_class.py
+++ python/car_class.py
@@ -1,6 +1,8 @@
-class Car:
-  speed: int = 0
-  started: bool = False
+class Vehicle:
+  def __init__(self, started: bool = False, speed: int = 0):
+    self.started = started
+    self.speed = speed
+    print("Vehicle created!")
 
   def start(self):
     self.started = True
@@ -17,10 +19,34 @@ class Car:
     self.speed = 0
     print('Halting')
 
+class Car(Vehicle):
+  trunk_open = False
+  def open_trunk(self):
+    if(not self.trunk_open):
+      self.trunk_open = True
+      return print('Trunk opened!')
+    print('Trunk already open!')
+
+  def close_trunk(self):
+    if(not self.trunk_open):
+      self.trunk_open = False
+      return print('Trunk closed!')
+    print('Trunk already closed!')
+
+class Motorcycle(Vehicle):
+  def __init__(self, center_stand_out = False):
+    self.center_stand_out = center_stand_out
+    super().__init__()
+
 car = Car()
 car.increase_speed(10)
 car.start()
 car.increase_speed(40)
 print(car.speed)
 
-dir(car)
+car1 = Car(True, 100)
+print(f'car1.started: {car1.started}, car1.speed: {car1.speed}')
+
+motorcycle = Motorcycle(True)
+print(motorcycle.center_stand_out)
+motorcycle.start()
