410ed85631bfb041db51d37e3cd9d93c62484c5b
DreSean-Sutton <dreseansutton@gmail.com>
2023-07-19 00:29:53 -0700

    Lists are interesting

--- python/datatype_tests.py
+++ python/datatype_tests.py
@@ -12,5 +12,44 @@ def tuples():
   print(f'length: {len(person)}') # 3
   return name, age, gender
 
+def lists():
+  my_list1 = [1, 2, 3, 4, 5]
+  my_list2 = ['a', 'b', 'c', 'd', 'e']
+  empty_list = []
+  range_list = list(range(1, 4)) # [1, 2, 3]
+  set_to_list = list({1, 2, 2, 2, 2, 3}) # [1, 2, 3]
+  my_list1.append(6) # [1, 2, 3, 4, 5, 6]
+  combine_lists = my_list1 + my_list2 # [1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e']
+  last = combine_lists.pop() # ['e']
+  first = combine_lists.pop(0) # [1]
+  del(my_list1[0]) # [2, 3, 4 , 5 , 6]
+  my_list1.remove(5) # [2, 3, 4, 6]
+  my_list2.append('e')
+  my_list2.count('e') # 2
+  result = 2 in my_list1 # True
+  my_list2.index('e') # 3
+  for n in my_list1:
+    print(n)
+  slice_list = my_list1[1:3]
+  print(slice_list) # [3, 4]
+  step_list = my_list1[::2] # [2, 4, 6]
+  unsorted_list = [5, 2, 8, 1, 7]
+  unsorted_list.sort() # [1, 2, 5, 7, 8]
+
+def sets():
+  my_set1 = {1, 2, 3, 4, 5}
+  my_set2 = {3, 4, 5, 6, 7}
+  my_set1.add(6) # {1, 2, 3, 4, 5, 6}
+  my_set1.remove(5) # {1, 2, 3, 4, 6}
+  my_set1.discard(6) # {1, 2, 3, 4}
+  my_set1.pop() # {1, 2, 3, 4, 6}
+  my_set1.clear() # set()
+  my_set1.update(range(1, 6)) # {1, 2, 3, 4, 5}
+  print(my_set1-my_set2) # {1, 2}
+  print(my_set2-my_set1) # {6, 7}
+
 print(check_instance(2, int)) # True
-tuples() # 28
+my_info = tuples()
+print(my_info) # ('Dre', 28,'male')
+print(lists())
+print(sets())
